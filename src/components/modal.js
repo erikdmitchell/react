68847035a93d0cba21d9eaff681414be08c984e3
Erik <erikdmitchell@gmail.com>
2017-02-17 09:20:03 -0500

    tweaked this thing with props and custom setup

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,47 +4,38 @@ import { Modal, Button } from 'react-bootstrap';
 class AppModal extends React.Component {
 	constructor(props, context) {
 		super(props, context);
-console.log('construct');
-		this.state = {showModal: false}	
+
+		this.state = {showModal: props.showModal}	
 	}
 	close() {
     	this.setState({ showModal: false })
 	}
-	open() {
+	open() {		
     	this.setState({ showModal: true })
 	}
-	render() {	
-console.log('foo');	
-console.log(this.state);			
+	render() {
+console.log(this.props);			
 		return (
-      <div>
-
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Text in a modal</h4>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-
-            <h4>Overflowing text to show scroll behavior</h4>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+			<div>
+				<Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+					<Modal.Header closeButton>
+						<Modal.Title>{this.props.title}</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						Modal body text/content.
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={this.close.bind(this)}>Close</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
 		)
 	}
 }
+
+AppModal.defaultProps= {
+	showModal: false,
+	title: 'Sample Modal Title'
+	}
 
 export default AppModal
